c0ba67f7d767ee0792a292cf12952f40da6d02a9
drasticroy <102560562+drasticroy@users.noreply.github.com>
2023-10-02 19:49:04 +0300

    config.js

--- config.js
+++ config.js
@@ -14,9 +14,9 @@ global.owner = [
 
 //global.pairingNumber = "254722961404" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['254740279985','254713163290'] 
+global.prems = ['254740279985','254713163290']
+global.allowed = ['254740279985','254713163290']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
