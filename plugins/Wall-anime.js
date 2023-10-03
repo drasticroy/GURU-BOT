303586227048eb42ec0133699b04146917493db9
drasticroy <102560562+drasticroy@users.noreply.github.com>
2023-10-03 11:33:05 +0300

    Wall-anime.js

--- plugins/Wall-anime.js
+++ plugins/Wall-anime.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 let handler = async (m, {command, conn, usedPrefix}) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/anime-${command}.json`)).data  
+let res = (await axios.get(`https://raw.githubusercontent.com/drasticroy/api/Guru/BOT-JSON/anime-${command}.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]   
 conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m)
 //conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 NEXT 🔄', `${usedPrefix + command}`]], m)    
