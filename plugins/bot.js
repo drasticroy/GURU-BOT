c7f72637ce7f4fed09ba213ff99cef4d8c999973
Recoder Drastic <102560562+drasticroy@users.noreply.github.com>
2023-10-06 06:57:42 +0300

    More Help where that came from

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
+let av = `./Assets/mp3/${pickRandom(["Yamete Kudasai", "Guru1", "step", "Guru2", "Guru3", "Guru4"])}.mp3`
 
-m.reply( `Hello ${taguser} Need help?  type /help `)
+m.reply( `Hello ${taguser} Need help😢?  type /help 😊 or contact Roy 😁on +254740279985 `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(bot|guru)$/i
+handler.customPrefix = /^(bot|drastic|roy|guru)$/i
 handler.command = new RegExp
 
 export default handler
