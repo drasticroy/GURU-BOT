6a41af332ab1f440c2f21fca86fb802b27f65e93
Recoder Drastic <102560562+drasticroy@users.noreply.github.com>
2023-10-12 16:56:15 +0300

    Update google-lens.js

--- plugins/google-lens.js
+++ plugins/google-lens.js
@@ -1,51 +1,44 @@
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import fetch from 'node-fetch'
-const {
-    generateSerpApiUrl
-} = await (await import('../lib/serpapi.js'));
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+import fetch from 'node-fetch';
 
-let handler = async (m, {
-    command,
-    usedPrefix,
-    conn,
-    text,
-    args
-}) => {
-    let [urutan] = args
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (!mime) throw 'The Media is not found😒'
-    let media = await q.download()
-    let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-    let link = await (isTele ? uploadImage : uploadFile)(media)
-    await m.reply(wait)
-    try {
-        const param = {
-            api_key: 'e3cdbff7d75ca5a40f0b75e97d6b28c53a50bbb0616f50c535ff85a1d031ded9',
-            engine: 'google_lens',
-            url: link
-        };
-        let all = await generateSerpApiUrl(param)
-        let data = all.visual_matches
-        if (!urutan) return m.reply("Input query!\n*Example:*\n.goolens [nomor]\n\n*Choose the numbers that exist*\n" + data.map((item, index) => `*${index + 1}.* ${item.title}`).join("\n"))
-        if (isNaN(urutan)) return m.reply("Input query!\n*Example:*\n.goolens [nomor]\n\n*Choose the numbers that exist*\n" + data.map((item, index) => `*${index + 1}.* ${item.title}`).join("\n"))
-        if (urutan > data.length) return m.reply("Input query!\n*Example:*\n.goolens [nomor]\n\n*Choose the numbers that exist*\n" + data.map((item, index) => `*${index + 1}.* ${item.title}`).join("\n"))
-        let out = data[urutan - 1]
-        let caption = `🔍 *[ RESUTLS😁 ]*
+import { generateSerpApiUrl } from '../lib/serpapi.js';
 
+let handler = async (m, { command, usedPrefix, conn, text, args }) => {
+  let [urutan] = args;
+  let q = m.quoted || m;
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime) throw 'The Media is not found 😒';
+  let media = await q.download();
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+  let link = await (isTele ? uploadImage : uploadFile)(media);
+  await m.reply(wait);
+  try {
+    const param = {
+      api_key: 'e3cdbff7d75ca5a40f0b75e97d6b28c53a50bbb0616f50c535ff85a1d031ded9',
+      engine: 'google_lens',
+      url: link,
+    };
+    
+    let all = await generateSerpApiUrl(param);
+    let data = all.visual_matches;
+    if (!urutan) return m.reply("Input query!\n*Example:*\n.goolens [nomor]\n\n*Choose the numbers that exist*\n" + data.map((item, index) => `*${index + 1}.* ${item.title}`).join("\n"));
+    if (isNaN(urutan)) return m.reply("Input query!\n*Example:*\n.goolens [nomor]\n\n*Choose the numbers that exist*\n" + data.map((item, index) => `*${index + 1}.* ${item.title}`).join("\n"));
+    if (urutan > data.length) return m.reply("Input query!\n*Example:*\n.goolens [nomor]\n\n*Choose the numbers that exist*\n" + data.map((item, index) => `*${index + 1}.* ${item.title}`).join("\n"));
+    let out = data[urutan - 1];
+    let caption = `🔍 *[ RESULTS 😁 ]*
 📋 *Description:* ${out.title || 'There is none'}
 📍 *Source:* ${out.source || 'There is none'}
 ⭐ *Link:* ${out.link || 'There is none'}
-📝 *Thumbnail:* ${out.thumbnail || 'There is none'}
-`;
+📝 *Thumbnail:* ${out.thumbnail || 'There is none'}`;
 
-        await m.reply(caption)
-    } catch (e) {
-        await m.reply(eror)
-    }
-}
-handler.help = ["goolens *[there is a number]*"]
-handler.tags = ["search"]
-handler.command = /^(googlelens)$/i
-export default handler
+    await m.reply(caption);
+  } catch (e) {
+    await m.reply(eror);
+  }
+};
+
+handler.help = ["goolens *[there is a number]*"];
+handler.tags = ["search"];
+handler.command = /^(googlelens)$/i;
+export default handler;
