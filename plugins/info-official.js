c93ac0e01989b24b169c82011230f714984030d2
Recoder Drastic <102560562+drasticroy@users.noreply.github.com>
2023-10-16 16:21:16 +0300

    Official

---
+++ plugins/info-official.js
@@ -0,0 +1,28 @@
+const handler = async (m, {conn, command}) => {
+  const str = `
+💜 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎 👻
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ *GITHUB*
+https://github.com/drasticroy
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ *INSTAGRAM*
+https://www.instagram.com/drastic_roy
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ *WHATSAPP*
+https://wa.me/+254740279985
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ *TIKTOK*
+https://www.tiktok.com/@drastic_roy
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+*If you have doubts, suggestions or questions just write to https://wa.me/+254740279985.*
+`.trim();
+
+  conn.sendHydrated(m.chat, str, wm, 'https://github.com/', '𝘽𝙤𝙩-𝙈𝘿', null, null, [
+    ['x'],
+    ['c'],
+    ['v'],
+  ], m);
+};
+
+handler.command = /^acc$/i;
+export default handler;
